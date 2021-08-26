3793b1d2f4045bf3f1099fb45bc431160fb99402
Nikhilesh-knk <nikhilesh.kurella@accolitedigital.com>
2021-08-26 16:50:43 +0530

    text added

--- formapp/src/app/components/rightbody/rightbody.component.ts
+++ formapp/src/app/components/rightbody/rightbody.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Text } from 'src/app/models/Text';
 @Component({
   selector: 'app-rightbody',
   templateUrl: './rightbody.component.html',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RightbodyComponent implements OnInit {
 
+  text: Text = {
+    fgid : 1,
+    name: "first name",
+    min_length: 2,
+    max_length: 30,
+    is_required: 0
+
+  };
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onCheckboxChange(): void {
+    this.text.is_required = this.text.is_required+1;
+    this.text.is_required = this.text.is_required%2;
+  }
 }
---
+++ formapp/src/app/models/Text.ts
@@ -0,0 +1,19 @@
+export class Text {
+    fgid: number;
+
+    name: string;
+    is_required: number;
+    max_length: number;
+    min_length: number;
+    constructor(
+        fgid: number,name: string,
+        is_required: number,
+        max_length: number,
+        min_length: number){
+        this.fgid = fgid;
+        this.name = name;
+        this.is_required = is_required;
+        this.max_length = max_length;
+        this.min_length = min_length;
+    }
+}
